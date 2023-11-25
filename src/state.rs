9203324c8c1e1b8801b89ab15ee1ec879b77356b
Congyu <leonwang998@gmail.com>
2023-11-25 12:37:41 +0800

    use btreemap for params

--- src/state.rs
+++ src/state.rs
@@ -6,7 +6,7 @@ use parking_lot::{Mutex, MutexGuard};
 use rand::{distributions::Alphanumeric, Rng};
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap,
+    collections::{BTreeMap, HashMap},
     path::{Path, PathBuf},
     sync::Arc,
 };
@@ -18,7 +18,7 @@ use url::Url;
 pub struct Route {
     pub id: String,
     pub url: Url,
-    pub params: HashMap<String, String>,
+    pub params: BTreeMap<String, String>,
 }
 
 #[derive(Deserialize)]
