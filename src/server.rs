a39cbf0c4690963db1497ec02946cb2a8dd7884a
Congyu WANG <leonwang998@gmail.com>
2024-05-18 22:52:10 +0800

    remove unnecessary cert_update_signal_rx.mark_unchanged

--- src/server.rs
+++ src/server.rs
@@ -68,7 +68,6 @@ pub async fn run_server(
             // new cert loaded
             Some(new_tls_acceptor) = tls_acceptor_rx.recv() => {
                 tls_acceptor = new_tls_acceptor;
-                cert_update_signal_rx.mark_unchanged();
                 cert_updating.store(false, Ordering::Release);
                 tracing::info!("cert updated");
                 continue;
